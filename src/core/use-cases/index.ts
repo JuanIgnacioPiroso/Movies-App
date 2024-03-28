6a57e6081fc3d727c972a0ee108921b025ad919c
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-03-28 01:24:07 -0300

    Add get-cast.use-case and update MovieDBMovie interface

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from './movies/popular.use-case';
 export * from './movies/top-rated.use-case copy';
 export * from './movies/upcoming.use-case';
 export * from './movie/get-by-id.use-case';
+export * from './movie/get-cast.use-case';
--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -3,10 +3,12 @@ import {useEffect, useState} from 'react';
 import * as UseCases from '../../core/use-cases';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
 import {FullMovie} from '../../core/entities/movie.entity';
+import {Cast} from '../../core/entities/cast.entity';
 
 export const useMovie = (movieID: number) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
 
   useEffect(() => {
     loadMovie();
@@ -15,11 +17,21 @@ export const useMovie = (movieID: number) => {
   const loadMovie = async () => {
     setIsLoading(true);
 
-    const fullMovie = await UseCases.getMovieByIdeUseCase(
+    const fullMoviePromise = await UseCases.getMovieByIdeUseCase(
       movieDBFetcher,
       movieID,
     );
+    const castPromise = await UseCases.getMovieCastUseCase(
+      movieDBFetcher,
+      movieID,
+    );
+
+    const [fullMovie, cast] = await Promise.all([
+      fullMoviePromise,
+      castPromise,
+    ]);
     setMovie(fullMovie);
+    setCast(cast);
     setIsLoading(false);
 
     console.log({fullMovie});
