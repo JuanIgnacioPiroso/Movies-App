514ffc906799bc1132c94b5176727fa9b6c3997a
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-03-28 00:19:04 -0300

    Add MovieHeader component to DetailsScreen

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -2,15 +2,21 @@ import {StackScreenProps} from '@react-navigation/stack';
 import {Text, View} from 'react-native';
 import {RootStackParams} from '../../navigation/Navigation';
 import {useMovie} from '../../hooks/useMovie';
+import {MovieHeader} from '../../components/movie/MovieHeader';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 
 export const DetailsScreen = ({route}: Props) => {
   const {movieID} = route.params;
-  const {} = useMovie(movieID);
+  const {isLoading, movie} = useMovie(movieID);
+
+  if (isLoading) {
+    return <Text>Loading</Text>;
+  }
   return (
     <View>
-      <Text> DetailsScreen </Text>
+      {/* Header */}
+      <MovieHeader movie={movie!} />
     </View>
   );
 };
