6681ef982de82e617cb6a85683fec6d878701eb3
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-03-28 01:45:48 -0300

    Add FullScreenLoader component to DetailsScreen and HomeScreen

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -5,6 +5,7 @@ import {useMovie} from '../../hooks/useMovie';
 import {MovieHeader} from '../../components/movie/MovieHeader';
 import {MovieDetails} from '../../components/movie/MovieDetails';
 import {ScrollView} from 'react-native-gesture-handler';
+import {FullScreenLoader} from '../../components/loaders/FullScreenLoader';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 
@@ -13,7 +14,7 @@ export const DetailsScreen = ({route}: Props) => {
   const {isLoading, movie, cast = []} = useMovie(movieID);
 
   if (isLoading) {
-    return <Text>Loading</Text>;
+    return <FullScreenLoader />;
   }
   return (
     <ScrollView>
