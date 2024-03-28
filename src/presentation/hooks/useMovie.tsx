15ef2921470caceb53c3bac1669b1d6e1b8d06ca
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-03-28 00:04:05 -0300

    Add movie getById use case and related interfaces

--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -1,14 +1,30 @@
 import {Text, View} from 'react-native';
 import {useEffect, useState} from 'react';
+import * as UseCases from '../../core/use-cases';
+import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
+import {FullMovie} from '../../core/entities/movie.entity';
 
 export const useMovie = (movieID: number) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState<FullMovie>();
 
   useEffect(() => {
     loadMovie();
   }, [movieID]);
 
-  const loadMovie = () => {};
+  const loadMovie = async () => {
+    setIsLoading(true);
 
-  return {};
+    const fullMovie = await UseCases.getMovieByIdeUseCase(
+      movieDBFetcher,
+      movieID,
+    );
+    setMovie(fullMovie);
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    movie,
+  };
 };
